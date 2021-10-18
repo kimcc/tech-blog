61ad3dcefe1cf6bff2d389287b0c5ee787073ee6
kimcc <15278020+kimcc@users.noreply.github.com>
2021-10-18 04:00:56 -0500

    clean up dashboard route so comment and user models aren't included in edit

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -50,20 +50,6 @@ router.get('/edit/:id', withAuth, (req, res) => {
       'title',
       'post_text',
       'created_at'
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
     ]
   })
     .then(dbPostData => {
